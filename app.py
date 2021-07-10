4d970d91a3335e418b3b5290a2169d4a9cc5066e
scan3ls <scan3ls@gmail.com>
2021-07-09 21:03:21 -0500

    Add mySQL

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'{environ.get("DATABASE_URL")}'
+db = SQLAlchemy(app)
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
 
 @app.route("/")
 def index():
-    return "Hello"
+    table_names = db.engine.table_names()
+    return jsonify({"Tables": table_names}) 
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
