f1ca9cd8e453da8910e39966f6f8382014360a15
scan3ls <scan3ls@gmail.com>
2021-07-15 15:01:52 -0500

    Change SQLALCHEMY configs

--- app.py
+++ app.py
@@ -7,7 +7,12 @@ from routes import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'{environ.get("DATABASE_URL")}'
-app.config['SQLALCHEMY_POOL_RECYCLE'] = 3600
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    'pool_recycle': 280,
+    'pool_timeout': 100,
+    'pool_pre_ping': True
+}
 CORS(app, resources={r"*": {"origins": "*"}})
 db.init_app(app)
 app.app_context().push()
