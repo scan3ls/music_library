9abadc6cc14a5ed79e1de8cceca8ca064c7acca7
scan3ls <scan3ls@gmail.com>
2021-07-15 18:11:53 -0500

    Add db rollback on query failure

--- routes/index.py
+++ routes/index.py
@@ -2,10 +2,17 @@ from flask import Flask, jsonify
 from routes import api
 from models.album import Album
 import json
+from models.db import db
 
 @api.route("/", methods=["GET"])
 def index():
-    albums = json.loads(str(Album.query.all()))
+    try:
+        albums = json.loads(str(Album.query.all()))
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        albums = json.loads(str(Album.query.all()))
+
     response = jsonify({"Albums": albums})
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response, 200
