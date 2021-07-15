ff116ed24843a3271be536b83c1a638f35576500
scan3ls <scan3ls@gmail.com>
2021-07-15 16:39:15 -0500

    Add try except

--- routes/artist.py
+++ routes/artist.py
@@ -2,10 +2,20 @@ from flask import Flask, jsonify
 from routes import api
 from models.artist import Artist
 import json
+from models.db import db
 
 @api.route("/artists", methods=["GET"])
 def artists_all():
-    artists = json.loads(str(Artist.query.all()))
+    print("*"*50)
+    print(db.session)
+    print("*"*50)
+
+    try:
+        artists = json.loads(str(Artist.query.all()))
+    except as e:
+        print(e)
+        artists = "artist"
+
     response = jsonify({"Artists": artists})
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response, 200
